1f356b930b543a22b2177ddadb56a51e29074237
Sunil Rao <sunil.rao@microsoft.com>
2020-05-10 22:11:04 -0700

    Add server.js script

---
+++ server.js
@@ -0,0 +1,15 @@
+console.log("Server starting");
+
+var express = require('express'),
+  app = express(),
+  port = 3000;
+
+app.use(express.static('build'));
+
+if (! module.parent) {
+  app.listen(port);
+}
+
+module.exports = app
+
+console.log("Server running on port " + port);
