ae47397885f0bd33b2e88a8220fc66df1683afce
Sunil Rao <sukarkal@microsoft.com>
2021-12-10 12:54:34 +0530

    Updates

---
+++ precache-manifest.3133869fd48f6433a07cd8347a03c557.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65a30d8e5f3fc9e2ba1fc239158600c6",
+    "url": "/sunilrao/index.html"
+  },
+  {
+    "revision": "d2bd25b187a1caca43fd",
+    "url": "/sunilrao/static/css/main.ba4c8268.chunk.css"
+  },
+  {
+    "revision": "a7a80c8046a0b3d74e3e",
+    "url": "/sunilrao/static/js/2.d0f9d098.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sunilrao/static/js/2.d0f9d098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2bd25b187a1caca43fd",
+    "url": "/sunilrao/static/js/main.00f3d8af.chunk.js"
+  },
+  {
+    "revision": "7af07634c1fb28bbc091",
+    "url": "/sunilrao/static/js/runtime-main.c1a8d8a9.js"
+  },
+  {
+    "revision": "b2ffb3a32c2e07826c887815599de6f3",
+    "url": "/sunilrao/static/media/octocat.d2ggb5d9.b2ffb3a3.png"
+  },
+  {
+    "revision": "5c14852dc31f28126170651247c3194a",
+    "url": "/sunilrao/static/media/twitter.324dwr32.5c14852d.png"
+  }
+]);
