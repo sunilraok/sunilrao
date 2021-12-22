17d1bdf77b80ffdb0bdc64399dd8e8c9815fde2b
Sunil Rao <sukarkal@microsoft.com>
2021-12-22 15:24:54 +0530

    Updates

---
+++ precache-manifest.e0e5d6381098d816097073fdf1318057.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae3e6bab9493ca18eb5006499152da5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd350d4700a1093aea2a",
+    "url": "/static/css/main.ba4c8268.chunk.css"
+  },
+  {
+    "revision": "a7a80c8046a0b3d74e3e",
+    "url": "/static/js/2.d0f9d098.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d0f9d098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd350d4700a1093aea2a",
+    "url": "/static/js/main.0281736a.chunk.js"
+  },
+  {
+    "revision": "c6f88e751c0dc9f8c8cc",
+    "url": "/static/js/runtime-main.79d6ca13.js"
+  },
+  {
+    "revision": "b2ffb3a32c2e07826c887815599de6f3",
+    "url": "/static/media/octocat.d2ggb5d9.b2ffb3a3.png"
+  },
+  {
+    "revision": "5c14852dc31f28126170651247c3194a",
+    "url": "/static/media/twitter.324dwr32.5c14852d.png"
+  }
+]);
